c37678aae432438434a34ec0182f547f4c1aa237
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2025-01-13 16:29:54 -0500

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TabContextProvider } from "../context/nav-context";
 import { Analytics } from "@vercel/analytics/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,6 +32,7 @@ export default function RootLayout({
         >
           <TabContextProvider>{children}</TabContextProvider>
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </>
